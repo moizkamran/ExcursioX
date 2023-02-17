bc0db4c97444bbb9b20a1113e004e0f61607e880
Talhaazhar50 <talhaazhar558@gmail.com>
2023-02-18 03:33:45 +0500

    chore: Dashboard

--- client/client/src/components/Popovers/forgot-password.jsx
+++ client/client/src/components/Popovers/forgot-password.jsx
@@ -1,14 +1,12 @@
+import "./forgot-password.css";
 import React from "react";
-import './forgot-password.css'
 
 function Popup(props) {
   return props.trigger ? (
     <div className="popup">
-      <div className="popup-inner">
-        {props.children}
-      </div>
+      <div className="popup-inner">{props.children}</div>
     </div>
   ) : null;
 }
 
-export default Popup
+export default Popup;
