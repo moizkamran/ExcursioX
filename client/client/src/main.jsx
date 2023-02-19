b4467ea57ecf1d55cd5235c937d945b1dd521ad0
Abdulmoeez Kamran <moiz.kamran28@gmail.com>
2023-02-19 08:50:17 +0500

    Fix

--- client/client/src/main.jsx
+++ client/client/src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <App />
   </React.StrictMode>,
 )
