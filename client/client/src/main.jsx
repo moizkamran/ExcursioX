f8bfce7e1962f589f52fb38b704f74a5017b2645
Abdulmoeez Kamran <moiz.kamran28@gmail.com>
2023-02-19 11:08:36 +0500

    Added React Hook for AuthContext

--- client/client/src/main.jsx
+++ client/client/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthContextProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </AuthContextProvider>
 )
