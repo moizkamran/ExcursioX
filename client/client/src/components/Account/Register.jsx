5655359dbbac40158e17c128a32aa7d8464d5d16
Abdulmoeez Kamran <moiz.kamran28@gmail.com>
2023-02-20 02:56:54 +0500

    Added actual form email fields not just props

--- client/client/src/components/Account/Register.jsx
+++ client/client/src/components/Account/Register.jsx
@@ -13,7 +13,10 @@ import {
   Checkbox,
   Button,
   Group,
-  Box
+  Box,
+  PasswordInput,
+  PasswordInputProps,
+  Anchor,
 } from "@mantine/core";
 
 import { useForm } from '@mantine/form';
@@ -93,6 +96,16 @@ const useStyles = createStyles((theme) => ({
 
 const Register = () => {
   const { classes } = useStyles();
+  const form = useForm({
+    initialValues: {
+      email: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
 
   return (
     <>
@@ -104,7 +117,28 @@ const Register = () => {
               Let's get you all set up!
               <Text c="dimmed">Please fill in the details correclty</Text>
             </Title>
-            
+            <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps('email')}
+        />
+
+        <Checkbox
+          mt="md"
+          label="I agree to sell my privacy"
+          {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+
+        
+      </form>
+    </Box>
           </Paper>
           
         </div>
