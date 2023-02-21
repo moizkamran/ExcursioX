20c38eb4d258135bba970f9348c0fe8b28eebed1
Talhaazhar50 <talhaazhar558@gmail.com>
2023-02-22 01:12:15 +0500

    dashboard body

---
+++ client/client/src/components/MainScreen/Test.jsx
@@ -0,0 +1,60 @@
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { Box } from "@mantine/core";
+import { useState } from "react";
+import { Testdata } from "./Testdata";
+
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+function Test() {
+  const [languages, setLanguages] = useState([
+    "Visas",
+    "Booking",
+    "Passengers",
+    "flights",
+  ]);
+
+  return (
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <Box
+        className="p-4"
+        style={{
+          marginTop: "30px",
+
+          width: "auto",
+        }}
+      >
+        <SortableContext
+          items={languages}
+          strategy={verticalListSortingStrategy}
+        >
+          {/* We need components that use the useSortable hook */}
+          {languages.map((language) => (
+            <Testdata key={language} id={language} />
+          ))}
+        </SortableContext>
+      </Box>
+    </DndContext>
+  );
+
+  function handleDragEnd(event) {
+    console.log("Drag end called");
+    const { active, over } = event;
+    console.log("ACTIVE: " + active.id);
+    console.log("OVER :" + over.id);
+
+    if (active.id !== over.id) {
+      setLanguages((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        console.log(arrayMove(items, activeIndex, overIndex));
+        return arrayMove(items, activeIndex, overIndex);
+      });
+    }
+  }
+}
+
+export default Test;
