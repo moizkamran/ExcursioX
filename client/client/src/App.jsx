96613eb8c42b9e2472f584cf7a67d0aefe028b0b
Abdulmoeez Kamran <moiz.kamran28@gmail.com>
2023-02-27 06:25:57 +0500

    Navigation Routes Protected

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,17 +1,20 @@
 import Dashboard from "./components/MainScreen/Dashboard";
 import Login from "./components/Account/Login";
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import Register from "./components/Account/Register";
+import { useAuthContext } from "./hooks/useAuthContext";
+
 
 export default function App() {
+  const {user} = useAuthContext()
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
+          <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+          <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
