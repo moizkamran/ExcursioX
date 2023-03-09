7436f5646a3388178f1efba1fec8a35e68e180f0
Talhaazhar50 <talhaazhar558@gmail.com>
2023-03-10 03:36:29 +0500

    Property Base

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,11 +1,11 @@
 import Dashboard from "./components/MainScreen/Dashboard";
+import Hotel from "./components/Pages/Hotel & Property/Hotel";
 import Login from "./components/Account/Login";
-import Hotel from "./components/Pages/Hotel & Property/Hotel"
+import PropertyScreen from "./components/Pages/Hotel & Property/AddProperty/PropertyScreen";
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import Register from "./components/Account/Register";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
-
 
 export default function App() {
   const {user} = useAuthContext()
@@ -17,6 +17,8 @@ export default function App() {
           <Route path="/hotel" element={<Hotel />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/AddProperty" element={<PropertyScreen />}></Route>
+
         </Routes>
       </BrowserRouter>
     </div>
