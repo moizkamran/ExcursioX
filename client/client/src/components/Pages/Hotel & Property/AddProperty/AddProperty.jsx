cc7b5d0406d53fa3b73566af3126973b9d2cad27
Abdulmoeez Kamran <70318057+moizkamran@users.noreply.github.com>
2023-03-11 01:59:11 +0000

    StateManagement Added for AddProperty (!NI)

--- client/client/src/components/Pages/Hotel & Property/AddProperty/AddProperty.jsx
+++ client/client/src/components/Pages/Hotel & Property/AddProperty/AddProperty.jsx
@@ -13,7 +13,116 @@ import {
 } from "@mantine/core";
 import PropertySelection from "./components/PropertySelection";
 
-const PropertyScreen = () => {
+
+
+// state = {
+//   step: 1,
+//   propertyType: '',
+//   propertyName: '',
+//   propertyContactPerson: '',
+//   propertyContactNumber: '',
+//   isCompanyOwned: false,
+//   useChannelManager: false,
+//   propertyAddress1: '',
+//   propertyAddress2: '',
+//   country: '',
+//   city: '',
+//   customName: '',
+//   bedrooms: 1,
+//   livingRoom: 0,
+//   bathroom: 1,
+//   bed1: '',
+//   bed1Q: 1,
+//   guestsStay: 1,
+//   enSuite: true,
+//   pernight: 0,
+//   parking: true,
+//   parkingType: '',
+//   parkingSite: '',
+//   parkingReservation: true,
+//   parkingPerDay: 0,
+//   breakfast: true,
+//   breakfastType: '',
+//   breakfastPrice: 0,
+//   language: '',
+//   language2: '',
+//   wifi: false,
+//   bar: false,
+//   airCondition: false,
+//   pool: false,
+
+// }
+
+// // Proceed to next step
+// nextStep = () => {
+//   const { step } = this.state;
+//   this.setState({
+//     step: step + 1
+//   });
+// }
+
+// // Back a steps
+// prevStep = () => {
+//   const { step } = this.state;
+//   this.setState({
+//     step: step - 1
+//   });
+// }
+
+// // Handle change
+// handleChange = input => e => {
+//   this.setState({[input]: e.target.value});
+// }
+
+const AddProperty = () => {
+
+  // // destruction of step
+  // const { step } = this.state;
+  // const { propertyType, propertyName, propertyContactPerson, propertyContactNumber, 
+  //   isCompanyOwned, useChannelManager, propertyAddress1, propertyAddress2, country, 
+  //   city, customName, bedrooms, livingRoom, bathroom, bed1, bed1Q, guestsStay, enSuite, pernight, 
+  //   parking, parkingType, parkingSite, parkingReservation, parkingPerDay, breakfast, breakfastType, 
+  //   breakfastPrice, language, language2, wifi, bar, airCondition, pool } = this.state;
+  // const values = { propertyType, propertyName, propertyContactPerson, propertyContactNumber, 
+  //   isCompanyOwned, useChannelManager, propertyAddress1, propertyAddress2, country, 
+  //   city, customName, bedrooms, livingRoom, bathroom, bed1, bed1Q, guestsStay, enSuite, pernight, 
+  //   parking, parkingType, parkingSite, parkingReservation, parkingPerDay, breakfast, breakfastType, 
+  //   breakfastPrice, language, language2, wifi, bar, airCondition, pool }
+
+  //   switch(step){
+  //     case 1:
+  //       return (
+  //         <PropertyDetails
+  //         nextStep={this.nextStep}
+  //         handleChange={this.handleChange}
+  //         values={values} />
+  //       )
+  //     case 2:
+  //       return (
+  //         <h1>Property Facilites</h1>
+  //       )
+  //     case 3:
+  //       return (
+  //         <h1>Property Layout</h1>
+  //       )
+  //     case 4:
+  //       return (
+  //         <h1>Property House Rules</h1>
+  //       )
+  //     case 5:
+  //       return (
+  //         <h1>Property Photos</h1>
+  //       )
+  //     case 6:
+  //       return (
+  //         <h1>Property Facilites</h1>
+  //       )
+  //     case 7:
+  //       return (
+  //         <h1>Property Facilites</h1>
+  //       )
+
+  //   }
   const [active, setActive] = useState(1);
   const nextStep = () =>
     setActive((current) => (current < 3 ? current + 1 : current));
@@ -62,4 +171,4 @@ const PropertyScreen = () => {
   );
 };
 
-export default PropertyScreen;
+export default AddProperty;
