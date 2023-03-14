69bb6fe1cd7968dbd8720228ce613a704a8bf9a6
Talhaazhar50 <talhaazhar558@gmail.com>
2023-03-15 00:48:29 +0500

    Booking Desgin

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,14 +1,16 @@
+import Booking from "./components/MainScreen/Booking";
 import Dashboard from "./components/MainScreen/Dashboard";
 import Hotel from "./components/Pages/Hotel & Property/Hotel";
 import Login from "./components/Account/Login";
+import PropertyLayoutAndPricing from "./components/Pages/Hotel & Property/AddProperty/components/PropertyLayoutAndPricing";
+import PropertyView from "./components/Pages/Hotel & Property/AddProperty/components/PropertyView";
 import React from "react";
 import Register from "./components/Account/Register";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
-import PropertyLayoutAndPricing from "./components/Pages/Hotel & Property/AddProperty/components/PropertyLayoutAndPricing";
 
 export default function App() {
-  const {user} = useAuthContext()
+  const { user } = useAuthContext()
   return (
     <div>
       <BrowserRouter>
@@ -17,7 +19,11 @@ export default function App() {
           <Route path="/hotel" element={<Hotel />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
-          <Route path="/AddProperty2" element={<PropertyLayoutAndPricing/>}></Route>
+          <Route path="/AddProperty2" element={<PropertyLayoutAndPricing />}></Route>
+          <Route path="/AddProperty3" element={<PropertyView />}></Route>
+          <Route path="/booking" element={<Booking />}></Route>
+
+
 
         </Routes>
       </BrowserRouter>
