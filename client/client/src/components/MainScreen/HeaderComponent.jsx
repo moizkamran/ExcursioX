f998397a9294f58d47e4661a8502bdec50fe98a0
Talhaazhar50 <talhaazhar558@gmail.com>
2023-03-16 00:52:02 +0500

    Navbar setting

--- client/client/src/components/MainScreen/HeaderComponent.jsx
+++ client/client/src/components/MainScreen/HeaderComponent.jsx
@@ -2,13 +2,13 @@ import "./stylesheets/Dashboard.css";
 import AddBooking from "../Pages/Hotel & Property/AddBooking/AddBooking";
 import AddProperty from "../Pages/Hotel & Property/AddProperty/AddProperty";
 import Body from "./Body";
+import Hotel from "../../components/Pages/Hotel & Property/Hotel";
 import MenuItems from "./MenuItems";
 import NavbarItems from "./NavbarItems";
 import React from "react";
 import logoImage from "../../assets/Logo1.png";
-import { AddRoom } from "../Pages/Hotel & Property/AddRoom/AddRoom";
 import { ScrollArea } from "@mantine/core";
-import Hotel from "../../components/Pages/Hotel & Property/Hotel";
+import { AddRoom } from "../Pages/Hotel & Property/AddRoom/AddRoom";
 
 export default function HeaderComponent() {
   return (
@@ -30,22 +30,22 @@ export default function HeaderComponent() {
           display: "flex",
           height: "calc(100vh - 80px)",
           overflowY: "hidden",
-        }} 
+        }}
       >
         <NavbarItems />
         {/* Interactive Component */}
         <div
-        style={{
-          borderRadius: "50px 0 0 0",
-          backgroundColor: "white",
-          marginLeft: "0px",
-          padding: "20px",
-          flex: 1,
-        }}
-      >
-        <ScrollArea.Autosize mah={"100%"} mx="auto">
-        <AddProperty/>
-        </ScrollArea.Autosize> </div>
+          style={{
+            borderRadius: "50px 0 0 0",
+            backgroundColor: "white",
+            marginLeft: "0px",
+            padding: "20px",
+            flex: 1,
+          }}
+        >
+          <ScrollArea.Autosize mah={"100%"} mx="auto">
+            <Hotel />
+          </ScrollArea.Autosize> </div>
       </div>
     </div>
   );
--- client/client/src/components/MainScreen/MenuItems.jsx
+++ client/client/src/components/MainScreen/MenuItems.jsx
@@ -1,80 +1,83 @@
+import "./stylesheets/Dashboard.css";
+import Hotel from "../Pages/Hotel & Property/Hotel";
 import React from "react";
-import {
-    Button,
-    Input,
-    Menu,
-    UnstyledButton,
-  } from "@mantine/core";
-  import "./stylesheets/Dashboard.css";
-  import {
-    IconCloudFog,
-    IconPlus,
-    IconSearch,
-    IconMessageDots,
-    IconBell,
-    IconMaximize,
-    IconWallet,
-  } from "@tabler/icons";
+import { CustomLink } from "./Dashboard";
 
+import {
+  Button,
+  Input,
+  Menu,
+  UnstyledButton,
+} from "@mantine/core";
+import {
+  IconCloudFog,
+  IconPlus,
+  IconSearch,
+  IconMessageDots,
+  IconBell,
+  IconMaximize,
+  IconWallet,
+} from "@tabler/icons";
 const MenuItems = () => {
   return (
+
     <div style={{ display: "flex", gap: "20px" }}>
-        <Menu>
-            <Menu.Target>
-              <Button
-                style={{
-                  backgroundColor: "white",
-                  color: "#07399E",
-                  display: "flex",
-                  alignItems: "center",
-                  borderRadius: "20px",
-                }}
-              >
-                <IconPlus size={16} color="blue" />
-              </Button>
-            </Menu.Target>
-
-            <Menu.Dropdown>
-              <Menu.Item>
-                <Input
-                  placeholder="Search..."
-                  variant="filled"
-                  icon={<IconSearch />}
-                  style={{ width: "300px" }}
-                />
-              </Menu.Item>
-
-              {/* Other items ... */}
-            </Menu.Dropdown>
-          </Menu>
-          <UnstyledButton className="headerIcon">
-          <IconMessageDots />
-          </UnstyledButton>
-          <UnstyledButton className="headerIcon">
-            <IconBell />
-          </UnstyledButton>
-          <UnstyledButton className="headerIcon">
-            <IconMaximize />
-          </UnstyledButton>
-          <UnstyledButton className="headerIcon">
-            <IconWallet />
-          </UnstyledButton>
-          <div
+      <Menu>
+        <Menu.Target>
+          <Button
             style={{
-              borderRadius: "20px",
+              backgroundColor: "white",
+              color: "#07399E",
               display: "flex",
               alignItems: "center",
-              background: "#2A303D",
-              padding: "10px",
-              fontFamily: "'Fredoka', sans-serif",
+              borderRadius: "20px",
             }}
           >
-            <IconCloudFog size={20} color={"#74FF79"} />
-            <span style={{ color: "white", marginLeft: "10px" }}>
-              Connected
-            </span>
-          </div>
-        </div>
+            <IconPlus size={16} color="blue" />
+          </Button>
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          <Menu.Item>
+            <Input
+              placeholder="Search..."
+              variant="filled"
+              icon={<IconSearch />}
+              style={{ width: "300px" }}
+            />
+          </Menu.Item>
+
+          {/* Other items ... */}
+        </Menu.Dropdown>
+      </Menu>
+      <UnstyledButton className="headerIcon">
+        <IconMessageDots />
+      </UnstyledButton>
+      <UnstyledButton className="headerIcon">
+        <IconBell />
+      </UnstyledButton>
+      <UnstyledButton className="headerIcon">
+        <IconMaximize />
+      </UnstyledButton>
+      <UnstyledButton className="headerIcon">
+        <IconWallet />
+      </UnstyledButton>
+      <div
+        style={{
+          borderRadius: "20px",
+          display: "flex",
+          alignItems: "center",
+          background: "#2A303D",
+          padding: "10px",
+          fontFamily: "'Fredoka', sans-serif",
+        }}
+      >
+        <IconCloudFog size={20} color={"#74FF79"} />
+        <span style={{ color: "white", marginLeft: "10px" }}>
+          Connected
+        </span>
+      </div>
+    </div>
   )
 }
 
