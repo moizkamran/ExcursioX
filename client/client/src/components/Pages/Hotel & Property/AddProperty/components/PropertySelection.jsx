cf503eda2005f4ea970bb46be018610bb4950ed1
Talhaazhar50 <talhaazhar558@gmail.com>
2023-03-21 01:11:28 +0500

    Detail page form implemented redux

--- client/client/src/components/Pages/Hotel & Property/AddProperty/components/PropertySelection.jsx
+++ client/client/src/components/Pages/Hotel & Property/AddProperty/components/PropertySelection.jsx
@@ -1,15 +1,9 @@
-import Image1 from "../../../../../assets/apartment.png";
 import HouseIcon from "../../../../../assets/propertyArtwork/house.png";
-import hotelIcon from "../../../../../assets/propertyArtwork/hotel.png";
-import TentIcon from "../../../../../assets/propertyArtwork/circus-tent.png";
-
-// Redux
-import { useDispatch } from 'react-redux';
-
+import Image1 from "../../../../../assets/apartment.png";
 import React, { useState } from "react";
-
-//Slice Import
-import { setPropertyType } from "../../../../../Redux/Slicers/propertySlice";
+import TentIcon from "../../../../../assets/propertyArtwork/circus-tent.png";
+import hotelIcon from "../../../../../assets/propertyArtwork/hotel.png";
+import { IconRotate2 } from "@tabler/icons";
 
 import {
   Button,
@@ -17,295 +11,289 @@ import {
   Image,
   Text,
 } from "@mantine/core";
-import { IconRotate2 } from "@tabler/icons";
 
 const PropertySelection = ({ onButtonClick }) => {
-  const dispatch = useDispatch();
 
-  const handlePropertyTypeChange = (event) => {
-    dispatch(setPropertyType(event.target.value));
-  };
 
   return (
     <>
-    <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+      <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
         {/* START OF APARTMENT QS */} <div>
-<Card style={{ borderRadius: "35px" }}>
-    <Card.Section
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 300,
-        height: 470,
-        backgroundColor: "#07399E",
-        mt: "md",
-        mb: "md",
-        padding: "xl",
-      }}
-    >
-      <Image
-        style={{
-          maxWidth: 240,
-          maxHeight: 240,
-          objectFit: 'cover',
-          marginTop: 10
-        }}
-        src={Image1}
-        alt="Random image"
-      />
-      <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24}}>Apartment</Text>
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "#002265",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          padding: 20,
-          height: 470,
-          maxHeight: 470,
-          alignItems: "center",
-          borderRadius: '35px',
-          borderTopLeftRadius: 50,
-          borderTopRightRadius: 50,
+          <Card style={{ borderRadius: "35px" }}>
+            <Card.Section
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: 300,
+                height: 470,
+                backgroundColor: "#07399E",
+                mt: "md",
+                mb: "md",
+                padding: "xl",
+              }}
+            >
+              <Image
+                style={{
+                  maxWidth: 240,
+                  maxHeight: 240,
+                  objectFit: 'cover',
+                  marginTop: 10
+                }}
+                src={Image1}
+                alt="Random image"
+              />
+              <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24 }}>Apartment</Text>
+              <div
+                style={{
+                  width: "100%",
+                  backgroundColor: "#002265",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  padding: 20,
+                  height: 470,
+                  maxHeight: 470,
+                  alignItems: "center",
+                  borderRadius: '35px',
+                  borderTopLeftRadius: 50,
+                  borderTopRightRadius: 50,
 
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-            fontSize: "20px",
-            fontWeight: 400,
-            fontFamily: 'Fredoka',
-            mt: "md",
-            mb: "md",
-            width: 250,
-            textAlign: 'center'
-          }}
-        >
-          Furnished & Self-catering
-          Where guests can
-          rent the entire place
-        </Text>
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: "20px",
+                    fontWeight: 400,
+                    fontFamily: 'Fredoka',
+                    mt: "md",
+                    mb: "md",
+                    width: 250,
+                    textAlign: 'center'
+                  }}
+                >
+                  Furnished & Self-catering
+                  Where guests can
+                  rent the entire place
+                </Text>
+              </div>
+            </Card.Section>
+
+          </Card>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px' }}>
+            <Button onClick={onButtonClick} leftIcon={<IconRotate2 />}
+              style={{ borderRadius: 35, backgroundColor: 'green' }}
+              name="propertyType"
+              value="Apartment"
+            >Quick Start</Button>
+
+
+
+          </div>
+        </div> {/* END OF APARTMENT QS */}
+        <div style={{ borderRight: "1px solid black" }} />
+        {/* START OF Homes QS */} <div>
+          <Card style={{ borderRadius: "35px" }}>
+            <Card.Section
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: 300,
+                height: 470,
+                backgroundColor: "#07399E",
+                mt: "md",
+                mb: "md",
+                padding: "xl",
+              }}
+            >
+              <Image
+                style={{
+                  maxWidth: 240,
+                  maxHeight: 240,
+                  objectFit: 'cover',
+                  marginTop: 10
+                }}
+                src={HouseIcon}
+                alt="Random image"
+              />
+              <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24 }}>Homes</Text>
+              <div
+                style={{
+                  width: "100%",
+                  backgroundColor: "#002265",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  padding: 20,
+                  alignItems: "center",
+                  borderRadius: '35px',
+                  borderTopLeftRadius: 50,
+                  borderTopRightRadius: 50,
+
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: "20px",
+                    fontWeight: 400,
+                    fontFamily: 'Fredoka',
+                    mt: "md",
+                    mb: "md",
+                    width: 250,
+                    height: 'max-content',
+                    paddingBottom: 10,
+                    textAlign: 'center'
+                  }}
+                >
+                  Properties like apartments,
+                  vacation homes, <br />
+                  villas, etc.
+                </Text>
+              </div>
+            </Card.Section>
+
+          </Card>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px' }}>
+
+          </div>
+        </div> {/* END OF Homes QS */}
+
+        {/* START OF APARTMENT QS */} <div>
+          <Card style={{ borderRadius: "35px" }}>
+            <Card.Section
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: 300,
+                height: 470,
+                backgroundColor: "#07399E",
+                mt: "md",
+                mb: "md",
+                padding: "xl",
+              }}
+            >
+              <Image
+                style={{
+                  maxWidth: 240,
+                  objectFit: 'cover',
+                  marginTop: 10
+                }}
+                src={hotelIcon}
+                alt="Random image"
+              />
+              <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24 }}>Hotel, B&Bs & More</Text>
+              <div
+                style={{
+                  width: "100%",
+                  backgroundColor: "#002265",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  padding: 20,
+                  alignItems: "center",
+                  borderRadius: '35px',
+                  borderTopLeftRadius: 50,
+                  borderTopRightRadius: 50,
+
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: "20px",
+                    fontWeight: 400,
+                    fontFamily: 'Fredoka',
+                    mt: "md",
+                    mb: "md",
+                    width: 250,
+                    textAlign: 'center'
+                  }}
+                >
+                  Properties like hotels,
+                  B&Bs, guest houses,
+                  hostels, condo hotels, etc.
+                </Text>
+              </div>
+            </Card.Section>
+
+          </Card>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px' }}>
+
+          </div>
+        </div> {/* END OF APARTMENT QS */}
+
+        {/* START OF APARTMENT QS */} <div>
+          <Card style={{ borderRadius: "35px" }}>
+            <Card.Section
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: 300,
+                height: 470,
+                backgroundColor: "#07399E",
+                mt: "md",
+                mb: "md",
+                padding: "xl",
+              }}
+            >
+              <Image
+                style={{
+                  maxWidth: 240,
+                  objectFit: 'cover',
+                  marginTop: 10
+                }}
+                src={TentIcon}
+                alt="Random image"
+              />
+              <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24 }}>Alternative Places</Text>
+              <div
+                style={{
+                  width: "100%",
+                  backgroundColor: "#002265",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  padding: 20,
+                  alignItems: "center",
+                  borderRadius: '35px',
+                  borderTopLeftRadius: 50,
+                  borderTopRightRadius: 50,
+
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: "20px",
+                    fontWeight: 400,
+                    fontFamily: 'Fredoka',
+                    mt: "md",
+                    mb: "md",
+                    width: 250,
+                    textAlign: 'center'
+                  }}
+                >
+                  Properties like boats, campgrounds, luxury tents, etc.
+                </Text>
+              </div>
+            </Card.Section>
+
+          </Card>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px' }}>
+
+          </div>
+        </div> {/* END OF APARTMENT QS */}
       </div>
-    </Card.Section>
-    
-  </Card>
-  <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',  height: '50px'}}>
-  <Button onClick={onButtonClick} leftIcon={<IconRotate2/>} 
-  style={{borderRadius: 35, backgroundColor: 'green'}}
-  name="propertyType"
-  value="Apartment"
-  onChange={handlePropertyTypeChange}
-  >Quick Start</Button>
 
-
-
-</div>
-  </div> {/* END OF APARTMENT QS */}
-  <div style={{ borderRight: "1px solid black" }} />
-  {/* START OF Homes QS */} <div>
-<Card style={{ borderRadius: "35px" }}>
-    <Card.Section
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 300,
-        height: 470,
-        backgroundColor: "#07399E",
-        mt: "md",
-        mb: "md",
-        padding: "xl",
-      }}
-    >
-      <Image
-        style={{
-          maxWidth: 240,
-          maxHeight: 240,
-          objectFit: 'cover',
-          marginTop: 10
-        }}
-        src={HouseIcon}
-        alt="Random image"
-      />
-      <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24}}>Homes</Text>
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "#002265",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          padding: 20,
-          alignItems: "center",
-          borderRadius: '35px',
-          borderTopLeftRadius: 50,
-          borderTopRightRadius: 50,
-
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-            fontSize: "20px",
-            fontWeight: 400,
-            fontFamily: 'Fredoka',
-            mt: "md",
-            mb: "md",
-            width: 250,
-            height: 'max-content',
-            paddingBottom: 10,
-            textAlign: 'center'
-          }}
-        >
-          Properties like apartments,
- vacation homes, <br/>
-villas, etc.
-        </Text>
-      </div>
-    </Card.Section>
-    
-  </Card>
-  <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',  height: '50px'}}>
-
-</div>
-  </div> {/* END OF Homes QS */}
-  
- {/* START OF APARTMENT QS */} <div>
-<Card style={{ borderRadius: "35px" }}>
-    <Card.Section
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 300,
-        height: 470,
-        backgroundColor: "#07399E",
-        mt: "md",
-        mb: "md",
-        padding: "xl",
-      }}
-    >
-      <Image
-        style={{
-          maxWidth: 240,
-          objectFit: 'cover',
-          marginTop: 10
-        }}
-        src={hotelIcon}
-        alt="Random image"
-      />
-      <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24}}>Hotel, B&Bs & More</Text>
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "#002265",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          padding: 20,
-          alignItems: "center",
-          borderRadius: '35px',
-          borderTopLeftRadius: 50,
-          borderTopRightRadius: 50,
-
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-            fontSize: "20px",
-            fontWeight: 400,
-            fontFamily: 'Fredoka',
-            mt: "md",
-            mb: "md",
-            width: 250,
-            textAlign: 'center'
-          }}
-        >
-          Properties like hotels, 
-B&Bs, guest houses, 
-hostels, condo hotels, etc.
-        </Text>
-      </div>
-    </Card.Section>
-    
-  </Card>
-  <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',  height: '50px'}}>
-
-</div>
-  </div> {/* END OF APARTMENT QS */}
-
-{/* START OF APARTMENT QS */} <div>
-<Card style={{ borderRadius: "35px" }}>
-    <Card.Section
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 300,
-        height: 470,
-        backgroundColor: "#07399E",
-        mt: "md",
-        mb: "md",
-        padding: "xl",
-      }}
-    >
-      <Image
-        style={{
-          maxWidth: 240,
-          objectFit: 'cover',
-          marginTop: 10
-        }}
-        src={TentIcon}
-        alt="Random image"
-      />
-      <Text style={{ color: "white", fontSize: 27, marginBottom: 20, fontFamily: 'Hammersmith One', marginTop: 24}}>Alternative Places</Text>
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "#002265",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          padding: 20,
-          alignItems: "center",
-          borderRadius: '35px',
-          borderTopLeftRadius: 50,
-          borderTopRightRadius: 50,
-
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-            fontSize: "20px",
-            fontWeight: 400,
-            fontFamily: 'Fredoka',
-            mt: "md",
-            mb: "md",
-            width: 250,
-            textAlign: 'center'
-          }}
-        >
-          Properties like boats, campgrounds, luxury tents, etc.
-        </Text>
-      </div>
-    </Card.Section>
-    
-  </Card>
-  <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',  height: '50px'}}>
-
-</div>
-  </div> {/* END OF APARTMENT QS */}
-  </div>
-  
     </>
   )
 }
--- client/client/src/main.jsx
+++ client/client/src/main.jsx
@@ -3,51 +3,58 @@ import "./styles.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import store from "./Redux/Store/store";
 import { MantineProvider } from "@mantine/core";
-import { AuthContextProvider } from "./context/AuthContext";
 import { Provider } from "react-redux";
+import { AuthContextProvider } from "./context/AuthContext";
 
 //REDUX STORE
-import store from "./Redux/Store/store"
 
 //main stylesheet import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<Provider store={store}>
-  <AuthContextProvider>
-    <MantineProvider
-      theme={{
-        colors : {
-          brand: [  '#e3eeff',  '#b4cdff',  '#84acfb',  '#548af7',  '#2669f4',  '#104fdb',  '#083eab',  '#022c7b',  '#001a4c',  '#00091e']
-        }, 
-        primaryColor: 'brand',
-        headings: {
-          // properties for all headings
-          fontWeight: 400,
-          fontFamily: 'Hammersmith One',
-        },
-        fontFamily: 'Fredoka',
-        components: {
-          Button: {
-            styles: {
-              root: {
-
-                // properties for all buttons
-                fontFamily: 'Fredoka',
-                fontWeight: 400,
-                borderRadius: 35,
-                backgroundColor: '#07399E',
-              }
-            }
-          }
-        }
-
-      }}
-    >
-
-      <App />
-      
-    </MantineProvider>
-  </AuthContextProvider>
-      </Provider>
+  <Provider store={store}>
+    <AuthContextProvider>
+      <MantineProvider
+        theme={{
+          colors: {
+            brand: [
+              "#e3eeff",
+              "#b4cdff",
+              "#84acfb",
+              "#548af7",
+              "#2669f4",
+              "#104fdb",
+              "#083eab",
+              "#022c7b",
+              "#001a4c",
+              "#00091e",
+            ],
+          },
+          primaryColor: "brand",
+          headings: {
+            // properties for all headings
+            fontWeight: 400,
+            fontFamily: "Hammersmith One",
+          },
+          fontFamily: "Fredoka",
+          components: {
+            Button: {
+              styles: {
+                root: {
+                  // properties for all buttons
+                  fontFamily: "Fredoka",
+                  fontWeight: 400,
+                  borderRadius: 35,
+                  backgroundColor: "#07399E",
+                },
+              },
+            },
+          },
+        }}
+      >
+        <App />
+      </MantineProvider>
+    </AuthContextProvider>
+  </Provider>
 );
