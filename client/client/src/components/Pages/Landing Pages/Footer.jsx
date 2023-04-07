c4bc5ffabd6cc4486b33bbf78e8984aa21feae3a
Abdulmoeez Kamran <70318057+moizkamran@users.noreply.github.com>
2023-04-07 02:50:33 +0000

    Footer Complete

--- client/client/src/components/Pages/Landing Pages/Footer.jsx
+++ client/client/src/components/Pages/Landing Pages/Footer.jsx
@@ -1,12 +1,14 @@
-import { Avatar, Container, Flex, Grid, Group, Image, Select, Text, Title } from '@mantine/core'
+import { Avatar, Button, Container, Flex, Grid, Group, Image, Select, Text, TextInput, Title } from '@mantine/core'
 
 import BSWhiteLogo from '../../../assets/booking-souq-white.svg'
 import { forwardRef, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { IconBrandFacebook, IconBrandInstagram, IconBrandLinkedin, IconBrandTwitter, IconMailOpened, IconRadar } from '@tabler/icons';
 
 
 const Footer = () => {
+  // Footer Locale selections manualy
   const regions = [
     {
       image: 'https://flagcdn.com/w40/ae.png',
@@ -68,6 +70,7 @@ const Footer = () => {
   const [region, setRegion] = useState('');
   console.log(region);
 
+  // AUTO DETECT REGION
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       async ({ coords }) => {
@@ -80,6 +83,7 @@ const Footer = () => {
     );
   }, []);
   
+  // For showing flags in select
   const SelectItem = forwardRef(({ image, label, ...others }, ref) => (
     <div ref={ref} {...others}>
       <Group noWrap>
@@ -93,27 +97,59 @@ const Footer = () => {
     </div>
   ));
 
+  // Footer Links Map
   const enterpriseLinks = [
-    { label: 'Overview', path: '/about-us' },
-    { label: 'Features', path: '/Features' },
-    { label: 'Solutions', path: '/Solutions' },
-    { label: 'Tutorials', path: '/Tutorials' },
-    { label: 'Pricing', path: '/Pricing' },
-    { label: 'Releases', path: '/Releases' },
-    { label: 'auw', path: '/Releases' },
-    { label: 'hellp', path: '/Releases' },
+    { label: 'Overview', path: '/overview' },
+    { label: 'Features', path: '/features' },
+    { label: 'Solutions', path: '/solutions' },
+    { label: 'Tutorials', path: '/tutorials' },
+    { label: 'Pricing', path: '/pricing' },
+    { label: 'Releases', path: '/releases' },
+    // COMPANY
+    { label: 'About us', path: '/about-us' },
+    { label: 'Careers', path: '/careers' },
+    { label: 'Partners', path: '/partners' },
+    { label: 'News', path: '/news' },
+    { label: 'Media Kit', path: '/media-kit' },
+    { label: 'Investor', path: '/investor' },
+    // Legal
+    { label: 'Terms', path: '/terms' },
+    { label: 'Privacy', path: '/privacy' },
+    { label: 'Cookies', path: '/cookies' },
+    { label: 'Licences', path: '/licences' },
+    { label: 'Security', path: '/security' },
+    { label: 'Sitemap', path: '/sitemap' },
+    // Resources
+    { label: 'Blog', path: '/blog' },
+    { label: 'Newsletter', path: '/newsletter' },
+    { label: 'Events', path: '/events' },
+    { label: 'Help Center', path: '/help-center' },
+    { label: 'Documentation', path: '/documentation' },
+    { label: 'Live Assist', path: '/live-assist' },
   ];
   
+  // Links Prefix
   const enterprisePrefix = '/enterprise';
-  
+
+  // vars
+   let radarNewsletter_Desc = "Our exclusive Radar Newsletter is your secret weapon to stay ahead of the competition. Get the latest insights and trends in the online travel industry straight to your inbox. Don't be just another player in the game, be a winner."
+  // FOOTER DEFAULT MARGIN (TOP)
+  const footerDefaultMargin = '100px';
 
   return (
     <>
-      <div style={{width: 'inherit', height: 20, backgroundColor: '#07399E', 
-      borderRadius: '21px 21px 0px 0px', marginTop: 500}}></div>
+     <div style={{
+      width: 'inherit',
+      height: 20,
+      backgroundColor: '#07399E', 
+      borderRadius: '21px 21px 0px 0px',
+      marginTop: `${footerDefaultMargin}`
+    }}></div>
     <Container bg={'#151515'} size={'100%'} h={'500px'} sx={{position: 'relative', color: 'white'}}>
-      <Flex p={50}>
 
+      <Flex p={50} justify={'space-between'}>
+        
+        {/* Branding and GeoLocale */}
         <Flex direction={'column'} w={300} gap={20}>
           <Image src={BSWhiteLogo} width={200}/>
           <Text ff={'Kumbh Sans'} fw={400}>A Innovative Robust OTA Power
@@ -129,7 +165,7 @@ const Footer = () => {
             dropdownPosition='bottom'
 
             data={regions}
-            w={180}
+            w={300}
             maxDropdownHeight={150}
             icon={<Image src={region? `https://flagcdn.com/w40/${region.toLowerCase()}.png` : ''} />}
             styles={{
@@ -151,53 +187,87 @@ const Footer = () => {
             }}
           />
         </Flex>
-
+        
+        {/* Links */}
         <Flex>
-        <Grid gutter={20}>
+        <Grid w={600}>
           <Grid.Col span={3}>
-            <Flex direction={'column'} gap={14}>
-            <Title ff={'Kumbh Sans'} fw={500} fz={15} c='rgba(255, 255, 255, 0.53)' >Company</Title>
-            {enterpriseLinks.map(({ label, path }) => (
-  <Text key={path} ff={'Kumbh Sans'} fw={600} fz={20} component={Link} to={`${enterprisePrefix}${path}`}>
-    {label}
-  </Text>
-))}
+            <Flex direction={'column'} gap={15}>
+            <Title ff={'Kumbh Sans'} fw={500} fz={15} c='rgba(255, 255, 255, 0.53)' >Product</Title>
+            {enterpriseLinks.slice(0, 6).map((item, index) => (
+              <Text key={index} ff={'Kumbh Sans'} fw={600} fz={20} component={Link} to={`${enterprisePrefix}${item.path}`}>
+                {item.label}
+              </Text>
+            ))}
             </Flex>
           </Grid.Col>
           <Grid.Col span={3}>
-          <Flex direction={'column'} gap={20}>
-              <Title ff={'Kumbh Sans'} fw={600} fz={25}>Company</Title>
-              <Text ff={'Kumbh Sans'} fw={400}>About Us</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Careers</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Contact Us</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Terms & Conditions</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Privacy Policy</Text>
+          <Flex direction={'column'} gap={15}>
+          <Title ff={'Kumbh Sans'} fw={500} fz={15} c='rgba(255, 255, 255, 0.53)' >Company</Title>
+              {enterpriseLinks.slice(6, 12).map((item, index) => (
+              <Text key={index} ff={'Kumbh Sans'} fw={600} fz={20} component={Link} to={`${enterprisePrefix}${item.path}`}>
+                {item.label}
+              </Text>
+            ))}
             </Flex>
           </Grid.Col>
           <Grid.Col span={3}>
-          <Flex direction={'column'} gap={20}>
-              <Title ff={'Kumbh Sans'} fw={600} fz={25}>Company</Title>
-              <Text ff={'Kumbh Sans'} fw={400}>About Us</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Careers</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Contact Us</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Terms & Conditions</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Privacy Policy</Text>
+          <Flex direction={'column'} gap={15}>
+          <Title ff={'Kumbh Sans'} fw={500} fz={15} c='rgba(255, 255, 255, 0.53)' >Legal</Title>
+              {enterpriseLinks.slice(12, 18).map((item, index) => (
+              <Text key={index} ff={'Kumbh Sans'} fw={600} fz={20} component={Link} to={`${enterprisePrefix}${item.path}`}>
+                {item.label}
+              </Text>
+            ))}
             </Flex>
           </Grid.Col>
           <Grid.Col span={3}>
-          <Flex direction={'column'} gap={20}>
-              <Title ff={'Kumbh Sans'} fw={600} fz={25}>Company</Title>
-              <Text ff={'Kumbh Sans'} fw={400}>About Us</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Careers</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Contact Us</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Terms & Conditions</Text>
-              <Text ff={'Kumbh Sans'} fw={400}>Privacy Policy</Text>
+          <Flex direction={'column'} gap={15}>
+          <Title ff={'Kumbh Sans'} fw={500} fz={15} c='rgba(255, 255, 255, 0.53)' >Resources</Title>
+          {enterpriseLinks.slice(18, 24).map((item, index) => (
+              <Text key={index} ff={'Kumbh Sans'} fw={600} fz={20} component={Link} to={`${enterprisePrefix}${item.path}`}>
+                {item.label}
+              </Text>
+            ))}
             </Flex>
           </Grid.Col>
         </Grid>
         </Flex>
 
+        {/* radarNewsletter */}
+        <Flex align={'flex-start'} direction={'column'} gap={20}>
+          <Flex justify={'center'} align={'center'}>
+            <Flex><IconRadar size={50} color='pink'/></Flex> <Text ff={'Kumbh Sans'} fw={600} fz={30} ml={10}>Radar Newsletter</Text>
+          </Flex>
+          <Text ff={'Kumbh Sans'} fw={300} fz={16} w={350}>{radarNewsletter_Desc}</Text>
+            <Flex>
+              <TextInput placeholder={'Enter your email'} w={250} styles={{
+                input: {
+                  backgroundColor: '#2F2F2F',
+                  color: 'white',
+                  height: 40,
+                  border: '1px solid #151515',
+                  borderRadius: 10,
+                },
+              }}/>
+              <Button ml={10} h={38} w={100} sx={{borderRadius: 10}}><IconMailOpened /></Button>
+
+            </Flex>
+        </Flex>  
+
       </Flex>
+
+      {/* Copyright and Social */}
+      <Flex justify={'space-between'} m={50} sx={{ borderTop: '1px solid #2F2F2F', paddingTop: '10px' }}>
+        <Text>BookingSouq 2023 ©️ All Rights Reserved</Text>
+        <Flex gap={20} >
+          <IconBrandFacebook />
+          <IconBrandTwitter />
+          <IconBrandInstagram />
+          <IconBrandLinkedin />
+        </Flex>
+      </Flex>
+
     </Container>
     </>
   )
