d8176cf6793dcc8792366e45993a2fa2e63b5df7
Abdulmoeez Kamran <moiz.kamran28@gmail.com>
2023-04-09 00:25:37 +0500

    newReq Utils Config

--- client/client/src/utils/newRequest.js
+++ client/client/src/utils/newRequest.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const baseURL = process.env.NODE_ENV === "production"
-  ? "https://winter-star-599.fly.dev/api/"
-  // : "http://localhost:8080/api/"; 
-  : "https://winter-star-599.fly.dev/api/";
+ ? "https://winter-star-599.fly.dev/api/"
+  : "http://localhost:8080/api/"
 
-console.log(baseURL);
+  console.log('%c⚙️ Server running in ', 'color: orange; font-weight: bold; background-color: black; padding: 4px;', process.env.NODE_ENV + ' mode');
+
 
 const newRequest = axios.create({
   baseURL,
