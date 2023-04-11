6a17ebc9a189cb4eedd84a751e8e901a4e3c2595
Abdulmoeez Kamran <moiz.kamran28@gmail.com>
2023-04-11 23:02:32 +0500

    Branding WIP

--- client/client/src/components/Pages/FunnelPages/Enterprise Signup/Pages/Branding.jsx
+++ client/client/src/components/Pages/FunnelPages/Enterprise Signup/Pages/Branding.jsx
@@ -1,7 +1,13 @@
-import { Flex, Image, Text } from '@mantine/core'
-import { IconArrowRight } from '@tabler/icons'
+import { Flex, Group, Image, Text, useMantineTheme, rem, TextInput, Button } from '@mantine/core'
+import { IconArrowRight, IconPhoto, IconSend, IconUpload, IconX } from '@tabler/icons'
 import { motion as m } from 'framer-motion'
-const Branding = () => {
+
+import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+
+import PhotosComponent from '../../../Hotel & Property/AddProperty/components/Functions/PhotosComponent';
+
+const Branding = (props) => {
+  const theme = useMantineTheme();
   return (
     <m.div
     style={{position: 'absolute', width: '65%', height: '70%'}}
@@ -22,27 +28,33 @@ vouchers, invoices and complete CRM branding</Text>
         <Flex direction={'column'} gap={20} mt={20}>
           {/* ENTERPRISE SIZE */}
           <Flex direction={'column'} gap={10}>
-              <Text fz={25} ff={'Kumbh Sans'} fw={500}>How big is the company?</Text>
+              <Text fz={25} ff={'Kumbh Sans'} fw={500}>Logo</Text>
+              <Flex gap={20} w={450}>
+              <LogoUpload  theme={theme} IMAGE_MIME_TYPE={IMAGE_MIME_TYPE} props={props} rem={rem}  />
+              <LogoUpload  theme={theme} IMAGE_MIME_TYPE={IMAGE_MIME_TYPE} props={props} rem={rem}  />
+              </Flex>
+              <Flex gap={20}>
+                <Flex gap={5} direction={'column'}>
+                  <Email heading={'Company Name'} placeholder={""}/>
+                  <Email heading={'Company Moto'} placeholder={""}/>
+                  <Email heading={'Company Address'} placeholder={""}/>
+                </Flex>
+                <Flex gap={5} direction={'column'}>
+                  <Email heading={'Company Phone'} placeholder={""}/>
+                  <Email heading={'How did you hear about us?'} placeholder={""}/>
+                </Flex>
+              </Flex>
+
           </Flex>
         </Flex>
       
   </Flex>
-  <Flex direction={'column'} gap={20} ml={50}>
-    <Flex gap={10} align={'center'}>
-      <Image src={'https://www.svgrepo.com/show/438412/court-round.svg'} width={60}/>
-      <Text fz={25} ff={'Kumbh Sans'} fw={600}>Legal Business Proof <br/> Document</Text>
+  <Flex direction={'column'} gap={20}>
+   <Flex bg={'#222323'} h={300} w={700} sx={{borderRadius: 20}}>
+    <Flex >
+      <Text align={'center'} justify={'center'} c={'white'}>ENSURE THE FOLLOWING</Text>
     </Flex>
-    <Text fz={18} ff={'Kumbh Sans'} w={400} fw={400} >
-    State law requires us to ask for valid
-    proof that you have rights to conduct
-    business with fields related to Aviation,
-    Transport, Accommodation, 
-    Travel & Tourism or Visa Services
-    </Text>
-    <Flex mt={-15} gap={8} align={'center'}>
-      <IconArrowRight color='#07399E'/>
-      <Text c={'#07399E'} >Learn More</Text>
-    </Flex> 
+   </Flex>
   </Flex>
 </Flex>
 
@@ -50,4 +62,47 @@ vouchers, invoices and complete CRM branding</Text>
   )
 }
 
-export default Branding
+
+    function LogoUpload({theme, IMAGE_MIME_TYPE, props, rem}) {
+      return (<Dropzone onDrop={files => console.log('accepted files', files)} onReject={files => console.log('rejected files', files)} maxSize={3 * 1024 ** 2} accept={IMAGE_MIME_TYPE} {...props}>
+      <Group position="center" spacing="xl" style={{
+    minHeight: rem(220),
+    pointerEvents: 'none',
+  }}>
+        <Dropzone.Accept>
+          <IconUpload size="3.2rem" stroke={1.5} color={theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]} />
+        </Dropzone.Accept>
+        <Dropzone.Reject>
+          <IconX size="3.2rem" stroke={1.5} color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]} />
+        </Dropzone.Reject>
+        <Dropzone.Idle>
+          <IconPhoto size="3.2rem" stroke={1.5} />
+        </Dropzone.Idle>
+
+        <div>
+          <Text size="xl" inline>
+            Drag images here or click to select files
+          </Text>
+          <Text size="sm" color="dimmed" inline mt={7}>
+            Attach as many files as you like, each file should not exceed 5mb
+          </Text>
+        </div>
+      </Group>
+    </Dropzone>);
+    }
+
+    function Email({placeholder, heading}) {
+      return (<Flex gap={20}direction={'column'}> 
+        <Text fz={15} ff={'Kumbh Sans'} fw={500}>{heading}</Text>
+        <TextInput placeholder={placeholder} w={250} mt={-15}styles={{
+    input: {
+      height: 30,
+      border: '1px solid #D0D5DD',
+      borderRadius: 10,
+      boxShadow: '0px 1.14159px 2.28317px rgba(16, 24, 40, 0.05)'
+    }
+    }} />
+        </Flex>);
+    }
+
+  export default Branding
